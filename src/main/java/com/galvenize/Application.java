aa8229ca866d90890751ec07c85c44353854282a
Vincent R. LoGiudice <vincent.r.logiudice@gmail.com>
2021-02-01 19:22:50 -0600

    initial commit

---
+++ src/main/java/com/galvenize/Application.java
@@ -0,0 +1,15 @@
+package com.galvenize;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+/* touch will create files in terminal
+* ie touch .gitignore
+*
+*
+*
+* */
