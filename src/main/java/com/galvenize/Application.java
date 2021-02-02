92e20815e1f1534dd7fc48422e0ce914955fe50f
Vincent R. LoGiudice <vincent.r.logiudice@gmail.com>
2021-02-02 13:20:59 -0600

    Gradle

--- src/main/java/com/galvenize/Application.java
+++ src/main/java/com/galvenize/Application.java
@@ -4,8 +4,41 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+    }
+
+    boolean isActive(String status) {
+        // Implement your solution
+        if (status.equals("active")) {
+            System.out.println("active");
+            return true;
+        } else {
+            System.out.println("false");
+            return false;
+        }
+        //boolean isActive = true;
+    }
+
+    int grade(int input) {
+        // Implement your solution
+        input = 67;
+        if (input < 59) {
+            System.out.println("F");
+        } else if (input <= 69 && input >= 60) {
+            System.out.println("D");
+        } else if (input <= 79 && input >= 70) {
+            System.out.println("C");
+        } else if (input <= 89 && input >= 80) {
+            System.out.println("B");
+        } else if (input <= 100 && input >= 90) {
+            System.out.println("A");
+        }
+
+        return input;
     }
 }
+
+
 
 /* touch will create files in terminal
 * ie touch .gitignore
@@ -16,4 +49,13 @@ public class Application {
 * run java name,java to run the java file
 * git push -u origin main //pushes from local to main branch
 *
+* Java primitives:
+* byte, short, int, long, float, double, char, and boolean.
+*
+*  git fetch updates updated repo locally from remote repo
+*  git merge origin/master - syncs with remote repo
+*  git pull is a shortcut command for the same thing.
+*  git pull origin/master
+*
 * */
+
