ed23925e9e8864d7a0bad28411de11d2444999c5
Vincent R. LoGiudice <vincent.r.logiudice@gmail.com>
2021-02-01 20:39:55 -0600

    java -jar setup

--- src/main/java/com/galvenize/Application.java
+++ src/main/java/com/galvenize/Application.java
@@ -11,8 +11,9 @@ public class Application {
 * ie touch .gitignore
 * create repo on github
 * git add -A
-*
+* git commit -m "type your notes for commit"
 * in terminal, use javac to compile a .java file
 * run java name,java to run the java file
+* git push -u origin main //pushes from local to main branch
 *
 * */
